47a1193e7af7739ec20447308d09c60c0a82e3ef
Cooper_W_04 <cwestervelt@csumb.edu>
2025-04-14 18:36:21 -0700

    "initial" commit with the User.java file very minimally worked on

---
+++ app/src/main/java/com/example/cst338project2randomgroups/database/entities/User.java
@@ -0,0 +1,45 @@
+package com.example.cst338project2randomgroups.database.entities;
+
+import java.util.ArrayList;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private ArrayList<User> positivePreferences;
+    private ArrayList<User> negativePreferences;
+
+    private boolean isStudent = false;
+    private boolean isTeacher = false;
+    private boolean isAdmin = false;
+    private User(String firstName, String lastName, String password, String status) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        if(status.equalsIgnoreCase("student")){
+            this.isStudent = true;
+            positivePreferences = new ArrayList<User>();
+            negativePreferences = new ArrayList<User>();
+        } else if(status.equalsIgnoreCase("teacher")){
+            this.isTeacher = true;
+        } else {
+            this.isAdmin = true;
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
