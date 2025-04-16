36886f63e953e3d5e070bfec38ccdf08680fee8f
Cooper_W_04 <cwestervelt@csumb.edu>
2025-04-16 10:55:37 -0700

    added some more methods and variables for Classroom.java

--- app/src/main/java/com/example/cst338project2randomgroups/database/entities/Classroom.java
+++ app/src/main/java/com/example/cst338project2randomgroups/database/entities/Classroom.java
@@ -1,5 +1,24 @@
+//TODO: make more interactions between classroom and user
 package com.example.cst338project2randomgroups.database.entities;
 
-public class Classroom {
+import java.util.ArrayList;
 
+public class Classroom {
+    private User teacher;
+    private String className;
+    private ArrayList<User> students;
+
+    public Classroom(User teacher, String className){
+        this.teacher = teacher;
+        this.className = className;
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(User student){
+        if(!student.getType().equalsIgnoreCase("student")){
+            //case that the user is not a student
+            return;
+        }
+        students.add(student);
+    }
 }
