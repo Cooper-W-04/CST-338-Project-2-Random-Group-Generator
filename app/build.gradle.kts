7bbcfb5c982cd76f8802024768643298d9d009a5
Cooper_W_04 <cwestervelt@csumb.edu>
2025-04-20 15:11:15 -0700

    made the correct gradle stuff in the app file, and changed some files to what they need to be, no major work in any

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -29,6 +30,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,7 +45,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    val room_version = "2.7.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 }
--- app/src/main/java/com/example/cst338project2randomgroups/database/AppDataBase.java
+++ app/src/main/java/com/example/cst338project2randomgroups/database/AppDataBase.java
@@ -1,4 +1,12 @@
 package com.example.cst338project2randomgroups.database;
 
-public class AppDataBase {
+import com.example.cst338project2randomgroups.database.entities.Classroom;
+import com.example.cst338project2randomgroups.database.entities.User;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class, Classroom.class}, version = 1, exportSchema = false)
+public abstract class AppDataBase extends RoomDatabase {
+
 }
