f2733eeeb56ebae28a231d0e466cf6d62922fe86
anntreasajojo <anntreasa.jojo@gmail.com>
2025-04-27 16:55:40 -0700

    Created a BEAUTIFUL page for the view all classes button. Used card view to make each class standout. Also added the buttons to view a selected class more closely.

---
+++ app/src/main/java/com/example/cst338project2randomgroups/StudentViewALLClassesActivity.java
@@ -0,0 +1,20 @@
+package com.example.cst338project2randomgroups;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StudentViewALLClassesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_student_view_allclasses);
+
+    }
+}
