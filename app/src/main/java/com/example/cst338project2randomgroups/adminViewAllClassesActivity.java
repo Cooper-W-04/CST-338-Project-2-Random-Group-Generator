bab94a793e9cbd61fe1e5e6a6577dbff9103be42
Sarah Wafa <sarahwafa6@gmail.com>
2025-04-28 13:47:20 -0700

    started the adminViewAllClassesActivity

--- app/src/main/java/com/example/cst338project2randomgroups/adminViewAllClassesActivity.java
+++ app/src/main/java/com/example/cst338project2randomgroups/adminViewAllClassesActivity.java
@@ -2,7 +2,13 @@ package com.example.cst338project2randomgroups;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cst338project2randomgroups.database.AppRepository;
+import com.example.cst338project2randomgroups.databinding.ActivityAdminViewAllClassesBinding;
+
 public class adminViewAllClassesActivity extends AppCompatActivity {
+    private ActivityAdminViewAllClassesBinding binding;
+    private AppRepository repository;
+
 
 
 }
