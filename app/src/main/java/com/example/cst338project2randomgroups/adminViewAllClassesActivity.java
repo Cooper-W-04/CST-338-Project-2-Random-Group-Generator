bd8d5ae641251449a02a3877ccbeee12642070af
Sarah Wafa <sarahwafa6@gmail.com>
2025-04-29 12:24:14 -0700

    still need to wire up button

--- app/src/main/java/com/example/cst338project2randomgroups/adminViewAllClassesActivity.java
+++ app/src/main/java/com/example/cst338project2randomgroups/adminViewAllClassesActivity.java
@@ -1,14 +1,36 @@
 package com.example.cst338project2randomgroups;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cst338project2randomgroups.database.AppRepository;
 import com.example.cst338project2randomgroups.databinding.ActivityAdminViewAllClassesBinding;
+import com.example.cst338project2randomgroups.databinding.ActivityMainBinding;
+
 
 public class adminViewAllClassesActivity extends AppCompatActivity {
-    private ActivityAdminViewAllClassesBinding binding;
+    private ActivityMainBinding binding;
     private AppRepository repository;
 
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        repository = AppRepository.getRepository(getApplication());
 
+        binding.viewAllClasses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
 
 }
