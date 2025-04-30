bc1af2a40d77fb773bf02526bf24e847d5a18077
Sarah Wafa <sarahwafa6@gmail.com>
2025-04-30 11:50:47 -0700

    created new xml for adding admin

--- app/src/main/java/com/example/cst338project2randomgroups/AddAdminActivity.java
+++ app/src/main/java/com/example/cst338project2randomgroups/AddAdminActivity.java
@@ -1,6 +1,14 @@
 package com.example.cst338project2randomgroups;
 
+import android.os.Bundle;
+
+import com.example.cst338project2randomgroups.database.AppRepository;
+import com.example.cst338project2randomgroups.databinding.ActivitySignupBinding;
+
 public class AddAdminActivity {
 
+    private ActivitySignupBinding binding;
+
+    private AppRepository repository;
 
 }
