b669ebb2ae479a03ed17a0bbcc0d72c8b2bfd72a
Sarah Wafa <sarahwafa6@gmail.com>
2025-04-30 11:18:46 -0700

    wired the buttons, issues

--- app/src/main/java/com/example/cst338project2randomgroups/MainActivity.java
+++ app/src/main/java/com/example/cst338project2randomgroups/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         adminViewClasses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = adminViewAllClassesActivity.adminAllClasssesIntentFactory(getApplicationContext());
+                Intent intent = admin.adminAllClasssesIntentFactory(getApplicationContext(), loggedInUserId);
                 startActivity(intent);
             }
         });
