d895aaedbea0cc8356b62a0d40175bc3a2fd08bc
Cooper_W_04 <cwestervelt@csumb.edu>
2025-05-01 17:54:58 -0700

    put the go back button in both of the pages i made. committing before i break something and taking a break for today

--- app/src/main/java/com/example/cst338project2randomgroups/ActivityAuthorityViewSelectedClassroom.java
+++ app/src/main/java/com/example/cst338project2randomgroups/ActivityAuthorityViewSelectedClassroom.java
@@ -2,11 +2,7 @@ package com.example.cst338project2randomgroups;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cst338project2randomgroups.databinding.ActivityAuthorityViewSelectedClassroomBinding;
 
