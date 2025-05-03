3847fcb4bb5ce4c596f45dd0695c06491aa34a1a
Cooper_W_04 <cwestervelt@csumb.edu>
2025-05-02 18:09:25 -0700

    fixed the authority viewing of a classroom page

--- app/src/main/java/com/example/cst338project2randomgroups/ActivityAuthorityViewSelectedClassroom.java
+++ app/src/main/java/com/example/cst338project2randomgroups/ActivityAuthorityViewSelectedClassroom.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cst338project2randomgroups.database.AppRepository;
 import com.example.cst338project2randomgroups.databinding.ActivityAuthorityViewSelectedClassroomBinding;
 
 public class ActivityAuthorityViewSelectedClassroom extends AppCompatActivity {
     ActivityAuthorityViewSelectedClassroomBinding binding;
+    private int classroomId;
+    public AppRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAuthorityViewSelectedClassroomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        int classroomId = getIntent().getIntExtra("CLASSROOM_ID", -1);
+        repository = AppRepository.getRepository(getApplication());
+
     }
 }
