41d61b5e568d9cf8934f1fc4f5d43ea925975b65
Cooper_W_04 <cwestervelt@csumb.edu>
2025-05-02 21:50:15 -0700

    changed the gradle file, as well as making the base skeleton class for the tests related to grabbing stuff from the database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,8 @@ dependencies {
     dependencies {
         implementation("androidx.cardview:cardview:1.0.0")
     }
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("androidx.test:core:1.5.0")
 }
