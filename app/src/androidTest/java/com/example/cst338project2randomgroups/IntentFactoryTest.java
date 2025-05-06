fbd41eeb8f54e59c97a7b6e46a2610830f9e0cc8
Sarah Wafa <sarahwafa6@gmail.com>
2025-05-06 12:58:48 -0700

    finished both unit tests and they are both running

--- app/src/androidTest/java/com/example/cst338project2randomgroups/IntentFactoryTest.java
+++ app/src/androidTest/java/com/example/cst338project2randomgroups/IntentFactoryTest.java
@@ -5,25 +5,31 @@ import static org.junit.Assert.assertEquals;
 import android.content.Context;
 import android.content.Intent;
 
-import androidx.test.espresso.internal.inject.InstrumentationContext;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-//Sarah's Tests
+
 
 @RunWith(AndroidJUnit4.class)
 public class IntentFactoryTest {
     //Sarah W
     @Test
-    public void checkAddAdminFactory(){
+    public void checkAddAdminFactory() {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         Intent intent = AddAdminActivity.AddAdminActivityIntentFactory(context);
 
         assertEquals(AddAdminActivity.class.getName(), intent.getComponent().getClassName());
     }
 
+    //Sarah w
+    @Test
+    public void checkEditGroupSizeFactory() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Intent intent = EditGroupSizeActivity.EditGroupSizeActivityIntentFactory(context);
+
+        assertEquals(EditGroupSizeActivity.class.getName(), intent.getComponent().getClassName());
+    }
 }
