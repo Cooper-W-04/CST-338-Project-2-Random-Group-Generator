407be9512816c8bd20078b5254ac7a8e3414e28a
Sarah Wafa <sarahwafa6@gmail.com>
2025-05-06 12:54:28 -0700

    finished 1/2 unit tests (got it running)

--- app/src/androidTest/java/com/example/cst338project2randomgroups/IntentFactoryTest.java
+++ app/src/androidTest/java/com/example/cst338project2randomgroups/IntentFactoryTest.java
@@ -1,14 +1,29 @@
 package com.example.cst338project2randomgroups;
 
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.espresso.internal.inject.InstrumentationContext;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 //Sarah's Tests
 
-
+@RunWith(AndroidJUnit4.class)
 public class IntentFactoryTest {
+    //Sarah W
+    @Test
+    public void checkAddAdminFactory(){
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Intent intent = AddAdminActivity.AddAdminActivityIntentFactory(context);
 
-    @Before
-    public void setup(){
-
+        assertEquals(AddAdminActivity.class.getName(), intent.getComponent().getClassName());
     }
 
 }
